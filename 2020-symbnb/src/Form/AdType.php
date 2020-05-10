64a609a9c6d7d5fe2f3a133864b2dac1730e018e
hseka <sekaherve@gmail.com>
2020-05-10 20:34:19 +0200

    Configurer les champs de formulaires. Les types de champs

--- 2020-symbnb/src/Form/AdType.php
+++ 2020-symbnb/src/Form/AdType.php
@@ -4,6 +4,11 @@ namespace App\Form;
 
 use App\Entity\Ad;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,14 +17,31 @@ class AdType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
-            ->add('slug')
-            ->add('price')
-            ->add('introduction')
-            ->add('content')
-            ->add('coverImage')
-            ->add('rooms')
+            ->add('title', TextType::class, $this->getConfiguration("Titre", "Saisir le titre...")
+            )
+            ->add('slug', TextType::class, $this->getConfiguration("URL de la page de la chambre", "Saisir l'url (automatique)"))
+          ->add('coverImage', UrlType::class, $this->getConfiguration("URL de l'image", "Donnez l'adresse d'une image qui donne envie..."))
+          ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Donnez une description globale de l'annonce"))
+          ->add('content', TextareaType::class, $this->getConfiguration("Description détaillée", "Saisir la description détaillée"))
+            ->add('rooms', IntegerType::class, $this->getConfiguration("Nombre de chambres", "Le nombre de chambres disponibles"))
+          ->add('price', MoneyType::class, $this->getConfiguration("Prix par nuit", "Indiquez le prix pour une nuit"))
         ;
+    }
+
+  /**
+   * Permet d'avoir la configuration des options d'un champ
+   * @param $label
+   * @param $placeholder
+   * @return array
+   */
+    private function getConfiguration($label, $placeholder)
+    {
+      return [
+        'label' => $label,
+        'attr' => [
+          'placeholder' => $placeholder
+        ]
+      ];
     }
 
     public function configureOptions(OptionsResolver $resolver)
