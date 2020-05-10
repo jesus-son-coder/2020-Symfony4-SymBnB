92093d0156a74ee66b047850d2a5b13a564c94fc
hseka <sekaherve@gmail.com>
2020-05-10 21:00:39 +0200

    Messages Flash pour notifier l'utilisateur

--- 2020-symbnb/src/Controller/AdController.php
+++ 2020-symbnb/src/Controller/AdController.php
@@ -51,6 +51,11 @@ class AdController extends AbstractController
       $manager->persist($ad);
       $manager->flush();
 
+      $this->addFlash(
+        'success',
+        "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+      );
+
       return $this->redirectToRoute('ads_show', [
         'slug' => $ad->getSlug()
       ]);
