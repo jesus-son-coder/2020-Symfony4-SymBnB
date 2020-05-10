88465be8d767018484eb01e9b4731c53968f80ad
hseka <sekaherve@gmail.com>
2020-05-10 10:48:03 +0200

    Création d'un objet "Ad" dans la méthode "load()" des Fixtures.
    Exécution de fixtures grâce à la commande :
    php bin/console doctrine:fixtures:load

--- 2020-symbnb/src/DataFixtures/AppFixtures.php
+++ 2020-symbnb/src/DataFixtures/AppFixtures.php
@@ -2,16 +2,27 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Ad;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
-
-        $manager->flush();
-    }
+  /**
+   * @param ObjectManager $manager
+   */
+  public function load(ObjectManager $manager)
+  {
+    $ad = new Ad();
+    $ad->setTitle("Titre de l'annonce")
+        ->setSlug("titre-de-l-annonce")
+        ->setCoverImage("http://placehold.it/1000x300")
+        ->setIntroduction("Bonjour à tous c'est une introduction")
+        ->setContent("<p>Je suis un contenu riche !</p>")
+        ->setPrice(80)
+        ->setRooms(3)
+    ;
+    $manager->persist($ad);
+    $manager->flush();
+  }
 }
