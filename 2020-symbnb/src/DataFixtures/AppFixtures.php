9cc4289840b7cc05a45d0f52029bf7a8269d9b02
hseka <sekaherve@gmail.com>
2020-05-10 12:29:40 +0200

    Enrichir la Fixture pour créer des images pour chaque annonce

--- 2020-symbnb/src/DataFixtures/AppFixtures.php
+++ 2020-symbnb/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -31,6 +32,14 @@ class AppFixtures extends Fixture
         ->setPrice(mt_rand(40,200))
         ->setRooms(mt_rand(1,5))
       ;
+
+      for($j=1; $j <= mt_rand(2,5); $j++) {
+        $image = new Image();
+        $image->setUrl($faker->imageUrl())
+          ->setCaption($faker->sentence())
+          ->setAd($ad);
+        $manager->persist($image);
+      }
       $manager->persist($ad);
     }
 
