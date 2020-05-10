4ee6b089f49c23f064a3e4bdf58856780ed31456
hseka <sekaherve@gmail.com>
2020-05-10 11:41:27 +0200

    Slugify: utilisation dans les Fixtures

--- 2020-symbnb/src/DataFixtures/AppFixtures.php
+++ 2020-symbnb/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Ad;
+use Cocur\Slugify\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -15,17 +16,19 @@ class AppFixtures extends Fixture
   public function load(ObjectManager $manager)
   {
     $faker = Factory::create('FR-fr');
+    $slugify = new Slugify();
 
     for($i = 1; $i <= 30; $i++) {
       $ad = new Ad();
 
       $title = $faker->sentence();
+      $slug = $slugify->slugify($title . $i);
       $coverImage = $faker->imageUrl(1000,350);
       $introduction = $faker->paragraph(2);
       $content = '<p>' . join('</p><p>', $faker->paragraphs(5)) . '</p>';
 
       $ad->setTitle($title)
-        ->setSlug("titre-de-l-annonce-n-$i")
+        ->setSlug($slug)
         ->setCoverImage($coverImage)
         ->setIntroduction($introduction)
         ->setContent($content)
